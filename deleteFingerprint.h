5032c05720828a4afc1c506fffdd04b6a2de1ebf
Victor Momodu <victoramomodu@gmail.com>
2023-08-11 12:13:03 +0100

    Deleted previous implementation

--- deleteFingerprint.h
+++
@@ -1,5 +0,0 @@
-#include <Adafruit_Fingerprint.h>
-
-uint8_t deleteFingerprint(Adafruit_Fingerprint fingerprintSensor, uint8_t deleteId);
-
-bool deleteAllFingerprint(Adafruit_Fingerprint fingerprintSensor);
--- verifyFingerprint.h
+++
@@ -1,4 +0,0 @@
-#include <Adafruit_Fingerprint.h>
-
-uint8_t verifyFingerprint(Adafruit_Fingerprint fingerprintSensor);
-int verifyAllFingerprint(Adafruit_Fingerprint fingerprintSensor);
